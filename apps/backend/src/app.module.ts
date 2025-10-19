cdc7bb6326c09c0cda23b710e000cf55614ccb74
Dhruv Kumar <dhruv.kum.04@gmail.com>
2025-10-19 12:14:23 +0530

    docker setup

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-
-
+import { ConfigModule } from '@nestjs/config';
+ 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${__dirname}../../../.env`
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/competitions/competitions.service.ts
+++ apps/backend/src/competitions/competitions.service.ts
@@ -1,3 +1,22 @@
-export class CompetitionsServices {
+import { ConflictException, HttpStatus } from "@nestjs/common";
 
+export class CompetitionsServices {
+    async createCompetitions(competition: {}) {
+        //push the info into the database
+
+        return { competitionID: 1 }; //this will be based on how the ID would be generated and returned
+    }
+
+    async registerForCompetition(email: string, idempotencyKey: string, competitionId: number) {
+        //mocking idempotency verification
+        if(idempotencyKey === 'aksjfhkjsfhs') throw new ConflictException('Request Already Exists.');
+
+        //if the key doesn't exist
+        //push it into redis queue
+        console.log('Registering for competition id ', competitionId, 'for user ', email);
+        return {
+            message: 'Registration Successful!',
+            statusCode: 201
+        }
+    }
 };
